cc8922cfbc93eeac8fcf627c58098bfe0fba79fb
njpoli <agratemail@gmail.com>
2023-09-02 03:42:11 -0400

    Match styles more closely to solution

--- app/components/ui/qr-code.tsx
+++ app/components/ui/qr-code.tsx
@@ -1,20 +1,19 @@
 import Image from "next/image";
-import qrCode from "/image-qr-code.png";
 
 function QRCode() {
   return (
-    <div className="flex flex-col items-center space-y-2 bg-[--color-white] text-center w-[250px] px-3 pt-3 pb-8 rounded-lg">
+    <div className="flex flex-col items-center space-y-2 bg-[--color-white] text-center w-[320px] px-4 pt-4 pb-8 rounded-[20px]">
       <Image
         src="/image-qr-code.png"
         alt="q r code"
-        height={250}
-        width={250}
-        className="rounded-lg"
+        height={320}
+        width={320}
+        className="rounded-[20px]"
       />
-      <h1 className="font-bold text-[--color-darkblue] text-lg">
+      <h2 className="font-bold text-[--color-darkblue] text-2xl">
         Improve your front-end skills by building projects
-      </h1>
-      <p className="font-normal text-[--color-grayishblue] leading-4 text-[15px]">
+      </h2>
+      <p className="font-normal text-[--color-grayishblue] leading-4 text-[15px] px-2">
         Scan the QR Code to visit Frontend Mentor and take your coding skills to
         the next level
       </p>
